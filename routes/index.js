d1b68fd0a15a7daf9dd183bc67a48c9c4c60706a
Calvin Chung <calvin@Calvins-MacBook-Pro.local>
2022-09-19 19:23:05 -0700

    API request route based off given stage id

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,16 @@ const axios = require("axios")
 const express = require('express');
 const router = express.Router();
 
+// 686488
 
 const API_KEY = process.env.API_KEY
-const url = `https://api.sportradar.com/formula1/trial/v2/us/sport_events/sr:stage:686488/probabilities.json?api_key=${API_KEY}`;
 
-router.get(`/`, (req, res) => {
-  const apiReq = axios.get(url)
+router.get(`/:stage`, (req, res) => {
+  const { stage } = req.params
+  const url = `https://api.sportradar.com/formula1/trial/v2/us/sport_events/sr:stage:${stage}/probabilities.json?api_key=${API_KEY}`;
+  axios.get(url)
     .then(response => {
-      res.send(response.data)
+      res.send(response.data);
     })
     .catch(err => {
       console.log(err);
